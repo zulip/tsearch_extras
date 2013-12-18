f82cbdfaa9803dee3ad49419c7fe0863e4880b95
Zev Benjamin <zev@mit.edu>
2013-12-18 17:06:45 -0500

    Name output parameters for ts_match_locs

--- tsearch_extras--1.0.sql
+++ tsearch_extras--1.0.sql
@@ -1,12 +1,12 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION tsearch_extras" to load this file. \quit
 
-CREATE FUNCTION ts_match_locs(IN regconfig, IN text, IN tsquery, OUT "offset" int4, OUT "len" int4)
+CREATE FUNCTION ts_match_locs(IN regconfig, IN text, IN tsquery, OUT int4, OUT int4)
 	RETURNS SETOF RECORD
         as 'MODULE_PATHNAME', 'ts_match_locs_byid'
         LANGUAGE C STRICT;
 
-CREATE FUNCTION ts_match_locs(IN text, IN tsquery, OUT "offset" int4, OUT "len" int4)
+CREATE FUNCTION ts_match_locs(IN text, IN tsquery, OUT int4, OUT int4)
 	RETURNS SETOF RECORD
         as 'MODULE_PATHNAME', 'ts_match_locs'
         LANGUAGE C STRICT;
